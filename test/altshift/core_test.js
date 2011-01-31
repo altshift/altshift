e702baef500b12612a4330b3154a50160cee61bb
Julien Polo <julien.polo@altshift.fr>
2011-01-31 12:04:39 +0100

    * more unit tests * jslint validation

--- test/altshift/core_test.js
+++ test/altshift/core_test.js
@@ -30,7 +30,7 @@ try {
 /*******************************************************************************
  * CoreTest
  ******************************************************************************/
-var CoreTest = vows.describe('Core').addBatch({
+var CoreTest = vows.describe('core module').addBatch({
     "require('core')": {
         topic: function () {
             return core;
@@ -55,6 +55,93 @@ var CoreTest = vows.describe('Core').addBatch({
             assert.isFunction(topic.module);
             assert.isFunction(topic.interface);
             assert.isFunction(topic.dict);
+        }
+    },
+    "isArray()": {
+        topic: function () {
+            return core.isArray;
+        },
+        'should return false for object, strings, number, boolean': function (topic) {
+            assert.equal(topic({}), false);
+            assert.equal(topic('foo bar'), false);
+            assert.equal(topic(1), false);
+            assert.equal(topic(true), false);
+            assert.equal(topic(null), false);
+            assert.equal(topic(undefined), false);
+            assert.equal(topic(function () {}), false);
+        },
+        'should return true for []': function (topic) {
+            assert.equal(topic([]), true);
+        },
+        'should return true for an Array subclass': function (topic) {
+            var ArraySub = function () {};
+            ArraySub.prototype = [];
+
+            assert.equal(topic(new ArraySub()), true);
+        }
+    },
+    "isString()": {
+        topic: function () {
+            return core.isString;
+        },
+        'should return false for object, arrays, number, boolean': function (topic) {
+            assert.equal(topic({}), false);
+            assert.equal(topic([]), false);
+            assert.equal(topic(1), false);
+            assert.equal(topic(true), false);
+            assert.equal(topic(null), false);
+            assert.equal(topic(undefined), false);
+            assert.equal(topic(function () {}), false);
+        },
+        'should return true for "my string"': function (topic) {
+            assert.equal(topic('my string'), true);
+        },
+        'should return true for a String subclass': function (topic) {
+            var StringSub = function () {};
+            StringSub.prototype = Object.create(String.prototype);
+
+            assert.equal(topic(new StringSub()), true);
+        }
+    },
+    "isFunction()": {
+        topic: function () {
+            return core.isFunction;
+        },
+        'should return false for arrays, strings, number, boolean': function (topic) {
+            assert.equal(topic('foo bar'), false);
+            assert.equal(topic([]), false);
+            assert.equal(topic(1), false);
+            assert.equal(topic(true), false);
+            assert.equal(topic(null), false);
+            assert.equal(topic(undefined), false);
+        },
+        'should return true for function () {}': function (topic) {
+            assert.equal(topic(function () {}), true);
+        }
+    },
+    "isObject()": {
+        topic: function () {
+            return core.isObject;
+        },
+        'should return false for arrays, strings, number, boolean': function (topic) {
+            assert.equal(topic('foo bar'), false);
+            assert.equal(topic(1), false);
+            assert.equal(topic(true), false);
+
+            assert.equal(topic(undefined), false);
+
+        },
+        'should return true for {} or [] or null or function() {}': function (topic) {
+            assert.equal(topic(null), true);
+            assert.equal(topic({}), true);
+            assert.equal(topic([]), true);
+            assert.equal(topic(function () {}), true);
+        },
+        'should return true for a Object subclass': function (topic) {
+            var ObjSub = function () {};
+            ObjSub.prototype = {};
+
+            assert.equal(topic(new ObjSub()), true);
         }
     }
 });
