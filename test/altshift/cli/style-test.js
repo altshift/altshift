387116879255f15575d883a15ef501fbf875727d
Julien Polo <julien.polo@altshift.fr>
2011-03-03 19:15:52 +0100

    * added style for console

---
+++ test/altshift/cli/style-test.js
@@ -0,0 +1,34 @@
+/*jslint nodejs: true, indent:4 */
+/**
+ * Generic Imports
+ */
+var vows = require('vows'),
+    assert = require('assert'),
+    path = require('path'),
+    env = require('../../_env');
+
+var __filenameTested = env.toFileTested(__filename);
+
+/**
+ * Imports
+ */
+var style = require(__filenameTested);
+
+/*******************************************************************************
+ * JSLint validation
+ ******************************************************************************/
+try {
+    require('lint').vows.createTest([__filename, __filenameTested]).export(module);
+} catch (e) {
+    console.warn('Warning: JSLint not found try `npm install lint`');
+}
+
+/*******************************************************************************
+ * StyleTest
+ ******************************************************************************/
+var StyleTest = vows.describe('style module').addBatch({
+
+
+});
+
+exports.StyleTest = StyleTest;
